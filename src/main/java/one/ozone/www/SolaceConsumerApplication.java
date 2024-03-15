9f819aaf910bc9bc25849ddaa0738937348edc72
Developer <developer@Developers-Mac-mini.local>
2024-03-15 13:02:00 +0530

    adding solace consumer application

---
+++ src/main/java/one/ozone/www/SolaceConsumerApplication.java
@@ -0,0 +1,13 @@
+package one.ozone.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolaceConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolaceConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/ozone/www/SolaceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package one.ozone.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolaceConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
